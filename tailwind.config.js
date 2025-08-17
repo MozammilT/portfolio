5bd6299e71abd8fdca5201d35ba5091f77d82441
Mozammil <mozammiltarique007@gmail.com>
2025-08-17 18:02:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fdc700",
+        background: "var(--background)",
+        "muted-foreground": "var(--muted-foreground)",
+      },
+    },
+  },
+  plugins: [],
+};

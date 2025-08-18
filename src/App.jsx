5293e770956e402ce4be1dda7bfd38668d0e3306
Mozammil <mozammiltarique007@gmail.com>
2025-08-18 14:22:23 +0530

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 
 function App() {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
+      <Hero />
+      <section className="min-h-screen"></section>
     </div>
   );
 }

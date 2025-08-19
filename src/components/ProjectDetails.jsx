720cad4c888e6b33ce2e345a450be3e496272c57
Mozammil <mozammiltarique007@gmail.com>
2025-08-20 01:54:40 +0530

    Add components for Project section

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,71 @@
+import { motion } from "motion/react";
+
+const ProjectDetails = ({
+  title,
+  description,
+  subDescription,
+  image,
+  tags,
+  href,
+  closeModal,
+}) => {
+  return (
+    <motion.div
+      className="fixed inset-0 z-50 flex items-center justify-center w-full h-full backdrop-blur-sm bg-black/50"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <motion.div
+        className="relative max-w-2xl max-h-[95vh] overflow-y-auto border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.5 }}
+        transition={{ duration: 0.3 }}
+      >
+        {/* Close button */}
+        <button
+          onClick={closeModal}
+          className="absolute p-2 rounded-lg top-5 right-5 bg-midnight hover:bg-gray-500"
+        >
+          <img src="/close.svg" className="w-6 h-6" />
+        </button>
+
+        {/* Modal content */}
+        <img src={image} alt={title} className="w-full rounded-t-2xl" />
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold text-white">{title}</h5>
+          <p className="mb-3 font-normal text-neutral-400">{description}</p>
+          {subDescription.map((subDesc, index) => (
+            <p key={index} className="mb-3 font-normal text-neutral-400">
+              {subDesc}
+            </p>
+          ))}
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex gap-3">
+              {tags.map((tag) => (
+                <img
+                  key={tag.id}
+                  src={tag.path}
+                  alt={tag.name}
+                  className="rounded-lg size-10 hover-animation"
+                />
+              ))}
+            </div>
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation"
+            >
+              View Project
+              <img src="/arrow-up.svg" className="size-4" />
+            </a>
+          </div>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default ProjectDetails;
